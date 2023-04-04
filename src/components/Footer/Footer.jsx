0080a248361c96ffd9bdd1167c2215a436635b02
SidZhoop <siddharth@zhoop.in>
2023-04-05 03:57:02 +0530

    completed home page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <h2>About</h2>
+          <p>
+            The point of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+          </p>
+        </div>
+        <div className={classes.col}>
+          <h2>Contacts</h2>
+          <span>Phone: +91 9562136789</span>
+          <span>GitHub: div-vik</span>
+        </div>
+        <div className={classes.col}>
+          <h2>Location</h2>
+          <span>Continent: Asia</span>
+          <span>Country: India</span>
+          <span>Current Location: Bangalore</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import PopularProperties from "../../components/PopularProperties/PopularProperties";
 import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <PopularProperties />
       <FeaturedProperties />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
